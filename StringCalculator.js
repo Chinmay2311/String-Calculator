98a29236fb983861c2751b06fc0f9f6b650be8ca
Chinmay Chavan <chinmay.chavan23@gmail.com>
2024-11-04 10:55:14 +0530

    Added a test case for negative numbers

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,6 +2,7 @@ function add(numbers){
     if(numbers === ""){
         return 0
     } else {
+        const negatives = [];
         let delimiter = /[,|\n]/;
         if (numbers.startsWith("//")) {
             const parts = numbers.split("\n", 2);
@@ -10,9 +11,18 @@ function add(numbers){
         }
       const nums = numbers.split(delimiter);
       let sum = 0
+      
       nums.forEach(num =>{
-        sum += parseInt(num, 10)
-      })
+        const n = parseInt(num, 10);
+        if (n < 0) {
+            negatives.push(n);
+        } else {
+            sum += n;
+        }
+    });
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
+    }
       return sum  
     }
     
